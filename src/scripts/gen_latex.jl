720e49768708ef4231360b4db7e9db1639697274
Suguru Yamamoto <sgryjp@gmail.com>
2019-06-23 10:49:29 +0900

    fix: Make gen_latex.jl runnable using Julia 0.7 or later
    REPLCompletions moved from Base to REPL since Julia 0.7.

--- src/scripts/gen_latex.jl
+++ src/scripts/gen_latex.jl
@@ -1,7 +1,13 @@
+if VERSION < v"0.7.0"
+  using Base.REPLCompletions: latex_symbols
+else
+  using REPL.REPLCompletions: latex_symbols
+end
+
 open(joinpath(dirname(@__FILE__), "..", "latex.ts"), "w") do f
   println(f, "export const latexSymbols = {")
   # sort by name length and name content
-  symbols = sort(collect(Base.REPLCompletions.latex_symbols))
+  symbols = sort(collect(latex_symbols))
   symbols = sort(symbols, by=x->length(x[1]))
   for (name, sym) in symbols
     println(f, "    '\\$name': '$sym',")
